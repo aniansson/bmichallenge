d335e5116bf072d2ceed046283195ee7a636e79a
aniansson <ebba@aniansson.se>
2017-01-17 15:20:05 +0100

    Add calculator, failing test

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,8 +1,16 @@
 angular.module('starter.controllers', [])
 
   .controller('BmiController', function($scope) {
-
-  })
+    $scope.data = {};
+    $scope.calculateBMI = function() {
+      var person = new Person ({
+        weight: $scope.data.weight,
+        height: $scope.data.height
+      });
+      person.calculate_bmi_met();
+      $scope.person = person;
+    };
+  });
 
   .controller('AboutController', function($scope) {
 
